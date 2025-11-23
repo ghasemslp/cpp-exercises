0edee268f741883124fc242f630556d6306915ae
ghasem_slp <159786102+ghasemslp@users.noreply.github.com>
2025-11-23 12:26:54 +0330

    New File
    برنامه ای بسازید که ، مکالمه کاربر و تعداد پیام کاربر را در یافت کند سپس قبض او را با مکالمه ساعتی 20 هزارتومان و پیامکی 20هزار ریال حساب کند

---
+++ Calculate_the_bill.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int call,sms, ghabz;
+    cout << "Saet Mokalme Ra Vard Konid: ";
+    cin >> call;
+    cout << "Tedad Payamk Haie Ersal Shode Ra Vard Konid: ";
+    cin >> sms;
+    ghabz = ((call / 60) * 20) + (sms * 2);
+    cout << "Ghabz Shoma = " << ghabz;
+    return 0;
+    
+}
+
+// ساخته شده توسط 
+// instagram = Ghasem_slp
+// GitHub = Ghasemslp
