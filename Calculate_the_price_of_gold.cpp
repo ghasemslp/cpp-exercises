c8c9226ca19a5fd3adcb96a6cfb9e5fe8e00408e
ghasem_slp <159786102+ghasemslp@users.noreply.github.com>
2025-11-23 12:25:40 +0330

    New file
    برنامه ای بسازید که ، وزن طلا را دریافت کند و قیمت آن را با گرمی 100 ه.ت محاسبه کند.

---
+++ Calculate_the_price_of_gold.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float weight,b;
+    cout << "Vazn Tala Ra Vard Konid: ";
+    cin >> weight;
+    b = (weight * 1000) * 100000;
+    cout << "Talay Shoma " << b << "Arzsh Mali Tomani Dard";
+    return 0;
+    
+}
+
+// ساخته شده توسط 
+// instagram = Ghasem_slp
+// GitHub = Ghasemslp
